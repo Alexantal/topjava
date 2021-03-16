bf9274c439ccf3f2a2e74edcf99e5a36acc5ca01
Alexantal <Alexantal@yandex.ru>
2021-03-16 09:20:00 +0300

    HW06 1.3.2_add AbstractMealController. JspMealController and MealRestController extend it.

--- src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -24,65 +24,52 @@ import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
 
 @Controller
-public class JspMealController {
+public class JspMealController extends AbstractMealController {
 
-    private final MealService mealService;
-
-    public JspMealController(MealService mealService) {
-        this.mealService = mealService;
+    protected JspMealController(MealService service) {
+        super(service);
     }
 
     @GetMapping("/meals")
-    public String getBetween(HttpServletRequest request) {
+    public String jspBetween(HttpServletRequest request) {
         LocalDate startDate = parseLocalDate(request.getParameter("startDate"));
         LocalDate endDate = parseLocalDate(request.getParameter("endDate"));
         LocalTime startTime = parseLocalTime(request.getParameter("startTime"));
         LocalTime endTime = parseLocalTime(request.getParameter("endTime"));
-        List<Meal> mealsDateFiltered = mealService.getBetweenInclusive(startDate, endDate, SecurityUtil.authUserId());
-        request.setAttribute("meals", MealsUtil.getFilteredTos(mealsDateFiltered, SecurityUtil.authUserCaloriesPerDay(), startTime, endTime));
+        request.setAttribute("meals", getBetween(startDate, startTime, endDate, endTime));
         return "meals";
     }
 
     @GetMapping("/meals/create")
-    public String getCreate(HttpServletRequest request) {
+    public String getJspCreate(HttpServletRequest request) {
         request.setAttribute("meal", new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000));
         return "mealForm";
     }
 
     @PostMapping("/meals/create")
-    public String postCreate(HttpServletRequest request) throws UnsupportedEncodingException {
+    public String postJspCreate(HttpServletRequest request) throws UnsupportedEncodingException {
         request.setCharacterEncoding("UTF-8");
-        int userId = SecurityUtil.authUserId();
         Meal meal = new Meal(
                 LocalDateTime.parse(request.getParameter("dateTime")),
                 request.getParameter("description"),
                 Integer.parseInt(request.getParameter("calories")));
         if (StringUtils.hasLength(request.getParameter("id"))) {
-            assureIdConsistent(meal, getId(request));
-            mealService.update(meal, userId);
+            update(meal, getId(request));
         } else {
-            checkNew(meal);
-            mealService.create(meal,userId);
+            create(meal);
         }
         return "redirect:/meals";
     }
 
     @GetMapping("/meals/update")
-    public String update(HttpServletRequest request){
-        request.setAttribute("meal", mealService.get(getId(request), SecurityUtil.authUserId()));
+    public String jspUpdate(HttpServletRequest request){
+        request.setAttribute("meal", get(getId(request)));
         return "mealForm";
     }
 
     @GetMapping("/meals/delete")
-    public String delete(HttpServletRequest request) {
-        int userId = SecurityUtil.authUserId();
-        int id = getId(request);
-        mealService.delete(id, userId);
+    public String jspDelete(HttpServletRequest request) {
+        delete(getId(request));
         return "redirect:/meals";
-    }
-
-    private int getId(HttpServletRequest request) {
-        String paramId = Objects.requireNonNull(request.getParameter("id"));
-        return Integer.parseInt(paramId);
     }
 }
